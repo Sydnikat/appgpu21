66a4dea23a5ed660c44442d0d5d33ac646601926
David Szalai <tonycalifor1@gmail.com>
2021-12-11 19:07:28 +0100

    Rewrite code so it can be tested in WSL

--- Assignment_4/ex_3/openacc_exercise.c
+++ Assignment_4/ex_3/openacc_exercise.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+#include <string.h>
 #include <inttypes.h>
 #include <math.h>
+#include <sys/time.h>
+#include <errno.h>
+#include <unistd.h>
 
-static uint64_t time_ns(void)
+static double time_ns(void)
 {
-	struct timespec ts;
+	struct timeval t0;
 
-	if (timespec_get(&ts, TIME_UTC) != TIME_UTC)
-	{
-		fputs("timespec_get failed!", stderr);
-		return 0;
-	}
-	return (uint64_t)1e9 * ts.tv_sec + ts.tv_nsec;
+	gettimeofday(&t0, NULL);
+
+	return (double)(t0.tv_sec) * 1e6 + (double)(t0.tv_usec);
 }
 
 
-int main(int argc, char* argv) {
+int main(int argc, char* argv[]) {
 	if (argc != 2)
 	{
 		printf("Not enough arguments...\n");
 		return -1;
 	}
-	size_t array_size = strtoull(argv[1], NULL, 10);
+	size_t array_size = atoi(argv[1]);
 
 	float* x = 0;
 	float* y = 0;
@@ -38,30 +38,45 @@ int main(int argc, char* argv) {
 	}
 
 	printf("Computing SAXPY sequentially...\n");
-	uint64_t start = time_ns();
+	double start = time_ns();
 
 	for (size_t i = 0; i < array_size; i++)
 	{
 		y[i] += a * x[i];
 	}
 
-	uint64_t end = time_ns() - start;
-	printf("Done! Time elapsed: (ns) = %I64u\t (s): %f\n\n", end, (float)end / 1e9);
-
-	printf("Computing SAXPY with OepnACC...\n");
-	uint64_t start = time_ns();
-
-	#pragma acc parallel loop copyin(x[0:array_size], y[0:array_size]) copyout(y[0:array_size])
-	for (size_t i = 0; i < array_size; i++)
-	{
-		y[i] += a * x[i];
-	}
-
-	uint64_t end = time_ns() - start;
-	printf("Done! Time elapsed: (ns) = %I64u\t (s): %f\n\n", end, (float)end / 1e9);
+	double end = time_ns() - start;
+	printf("Done! Time elapsed: (ms) = %d\t (s): %f\n\n", (int)end, end / 1e6);
 
 	free(x);
 	free(y);
+
+	float* g_x = 0;
+	float* g_y = 0;
+	a = 1.6f;
+
+	g_x = (float*)malloc(array_size * sizeof(float));
+	g_y = (float*)malloc(array_size * sizeof(float));
+
+	for (size_t i = 0; i < array_size; i++) {
+		g_x[i] = (float)i;
+		g_y[i] = 1.0f;
+	}
+
+	printf("Computing SAXPY with OepnACC...\n");
+	start = time_ns();
+
+	#pragma acc parallel loop copyin(g_x[0:array_size]) copyout(g_y[0:array_size])
+	for (size_t i = 0; i < array_size; i++)
+	{
+		g_y[i] += a * g_x[i];
+	}
+
+	end = time_ns() - start;
+	printf("Done! Time elapsed: (ms) = %d\t (s): %f\n\n", (int)end, end / 1e6);
+
+	free(g_x);
+	free(g_y);
 
 	return 0;
 }
